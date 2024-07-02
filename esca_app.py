82f393b7f392a4eb16772ed25779715e1e55cd38
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-07-02 15:23:59 +0200

    Update esca_app.py

--- esca_app.py
+++ esca_app.py
@@ -6,7 +6,6 @@ from tensorflow.keras.utils import get_custom_objects
 from PIL import Image
 import numpy as np
 
-# Define the custom focal loss function
 def focal_loss(gamma=2., alpha=0.25):
     def focal_loss_fixed(y_true, y_pred):
         y_true = tf.cast(y_true, tf.float32)
@@ -17,7 +16,6 @@ def focal_loss(gamma=2., alpha=0.25):
         return tf.reduce_mean(focal_loss_value)
     return focal_loss_fixed
 
-# Register the custom loss function
 get_custom_objects().update({"focal_loss_fixed": focal_loss()})
 
 # Download the model file from Hugging Face
@@ -26,14 +24,11 @@ model_filename = "grapeleaf_classifier.keras"
 
 model_file = hf_hub_download(repo_id=model_id, filename=model_filename)
 
-# Load the Keras model
 model = load_model(model_file, custom_objects={"focal_loss_fixed": focal_loss()}, compile=False)
 
-# Compile the model after loading
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001, clipvalue=1.0)
 model.compile(optimizer=optimizer, loss=focal_loss(), metrics=['accuracy'])
 
-# Define a function to preprocess the image
 def preprocess_image(image):
     image = image.convert("RGB")  # Convert to RGB to ensure 3 channels
     image = image.resize((150, 150))  # Resize to the input size expected by your model
@@ -41,7 +36,6 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)  # Add batch dimension
     return image
 
-# Sidebar with description
 st.sidebar.title("About Esca Disease")
 st.sidebar.write("""
 Esca is a serious fungal disease that affects grapevines, causing significant damage to vineyards. Early detection of Esca can help in managing and controlling its spread, ensuring healthier vineyards and better grape yields.
@@ -56,7 +50,6 @@ Esca is a serious fungal disease that affects grapevines, causing significant da
   - **Accuracy**: The overall model accuracy is 92%, so the model correctly predicts both Esca and healthy leaves 92% of the time.
 """)
 
-# Main page for image upload and detection
 st.title("Esca Disease Detection in Grapevine Leaves")
 st.write("Upload a grape leaf image (JPG or PNG) to detect if it shows signs of Esca infection.")
 
@@ -68,13 +61,9 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded Image.', use_column_width=True)
 
     if st.button("Detect Esca"):
-        # Preprocess the image
         img = preprocess_image(image)
-
-        # Perform the classification
         result = model.predict(img)
 
-        # Interpret the result
         label = 'esca' if result[0][0] < 0.5 else 'healthy'
 
         if label == 'esca':
